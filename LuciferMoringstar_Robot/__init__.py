228db3f0ab3fd5754c5e53949812ba44b5b89bbd
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-01-08 12:28:46 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,12 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [TEDZO SIR](
+@tedzopro)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Tutorial : [Click Me](https://chat.whatsapp.com/KzPohCK51Tt0Ok4Gt0N12T)
 """
